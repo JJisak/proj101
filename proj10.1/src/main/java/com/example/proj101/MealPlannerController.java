6ca8d74612006ea5a88a6dad64d521c17a17be12
JJisak <76518907+JJisak@users.noreply.github.com>
2023-06-07 19:20:16 -0400

    MealPlannerController.java

--- proj10.1/src/main/java/com/example/proj101/MealPlannerController.java
+++ proj10.1/src/main/java/com/example/proj101/MealPlannerController.java
@@ -1,11 +1,11 @@
 package com.example.proj101;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class MealPlannerController {
@@ -14,44 +14,59 @@ public class MealPlannerController {
 
     @GetMapping("/mealplanner/week")
     public ResponseEntity<WeekResponse> getWeekMeals(
-            @RequestParam(value = "numCalories", required = false) String numCalories,
+            @RequestParam(value = "numCalories", required = false, defaultValue = "2000") String numCalories,
             @RequestParam(value = "diet", required = false) String diet,
             @RequestParam(value = "exclusions", required = false) String exclusions) {
 
-        if (numCalories == null) {
-            numCalories = "2000";
-        }
-        if (diet == null) {
-            diet = "none";
-        }
-        if (exclusions == null) {
-            exclusions = "";
-        }
-
-        String url = SPOONACULAR_API_URL + "generate?apiKey=" + SPOONACULAR_API_KEY +
-                "&timeFrame=week&targetCalories=" + numCalories +
-                "&diet=" + diet + "&exclude=" + exclusions;
+        String url = UriComponentsBuilder.fromHttpUrl(SPOONACULAR_API_URL + "generate")
+                .queryParam("apiKey", SPOONACULAR_API_KEY)
+                .queryParam("timeFrame", "week")
+                .queryParam("targetCalories", numCalories)
+                .queryParam("diet", diet)
+                .queryParam("exclude", exclusions)
+                .toUriString();
 
         RestTemplate restTemplate = new RestTemplate();
-        WeekResponse weekResponse = restTemplate.getForObject(url, WeekResponse.class);
+        WeekResponse weekResponse;
 
-        return new ResponseEntity<>(weekResponse, HttpStatus.OK);
+        try {
+            ResponseEntity<WeekResponse> response = restTemplate.getForEntity(url, WeekResponse.class);
+            if (response.getStatusCode() == HttpStatus.OK) {
+                weekResponse = response.getBody();
+                return new ResponseEntity<>(weekResponse, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
 
     @GetMapping("/mealplanner/day")
     public ResponseEntity<DayResponse> getDayMeals(
-            @RequestParam(value = "numCalories", required = false) String numCalories) {
+            @RequestParam(value = "numCalories", required = false, defaultValue = "2000") String numCalories) {
 
-        if (numCalories == null) {
-            numCalories = "2000";
-        }
-
-        String url = SPOONACULAR_API_URL + "generate?apiKey=" + SPOONACULAR_API_KEY +
-                "&timeFrame=day&targetCalories=" + numCalories;
+        String url = UriComponentsBuilder.fromHttpUrl(SPOONACULAR_API_URL + "generate")
+                .queryParam("apiKey", SPOONACULAR_API_KEY)
+                .queryParam("timeFrame", "day")
+                .queryParam("targetCalories", numCalories)
+                .toUriString();
 
         RestTemplate restTemplate = new RestTemplate();
-        DayResponse dayResponse = restTemplate.getForObject(url, DayResponse.class);
+        DayResponse dayResponse;
 
-        return new ResponseEntity<>(dayResponse, HttpStatus.OK);
+        try {
+            dayResponse = restTemplate.getForObject(url, DayResponse.class);
+            if (dayResponse != null) {
+                return new ResponseEntity<>(dayResponse, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
