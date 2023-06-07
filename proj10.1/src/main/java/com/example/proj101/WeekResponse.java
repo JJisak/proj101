39b1280f78979334025274759cfc45e09bc235bf
JJisak <76518907+JJisak@users.noreply.github.com>
2023-06-07 17:35:25 -0400

    WeekResponse.java

--- proj10.1/src/main/java/com/example/proj101/WeekResponse.java
+++ proj10.1/src/main/java/com/example/proj101/WeekResponse.java
@@ -14,8 +14,9 @@ public class WeekResponse {
     }
 }
 
-class DayResponse {
+ class DayResponse {
     private List<Meal> meals;
+    private Nutrients nutrients;
 
     public List<Meal> getMeals() {
         return meals;
@@ -23,6 +24,53 @@ class DayResponse {
 
     public void setMeals(List<Meal> meals) {
         this.meals = meals;
+    }
+
+    public Nutrients getNutrients() {
+        return nutrients;
+    }
+
+    public void setNutrients(Nutrients nutrients) {
+        this.nutrients = nutrients;
+    }
+}
+
+ class Nutrients {
+    private double calories;
+    private double carbohydrates;
+    private double fat;
+    private double protein;
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+
+    public double getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public void setCarbohydrates(double carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
     }
 }
 
